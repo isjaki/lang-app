061b5523005115f86272ba0f3cfbe8dea0086bb0
Aleksei Usov <alekseiusov@MacBook-Pro-Alfa.local>
2022-10-05 21:14:55 +0600

    update exception handling

--- src/main/java/com/langapp/common/exception/CommonExceptionHandler.java
+++ src/main/java/com/langapp/common/exception/CommonExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.langapp.common.exception;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,22 +9,28 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class CommonExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public Map<String, Object> handleMethodArgumentNotValidException(HttpServletRequest req, HttpServletResponse res, MethodArgumentNotValidException e) {
-        return buildResponse(req, res, e, HttpStatus.BAD_REQUEST.value());
+        List<String> messages = e.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        return buildResponse(req, res, e, HttpStatus.BAD_REQUEST.value(), messages);
     }
 
     public Map<String, Object> buildResponse(HttpServletRequest req, HttpServletResponse res, Exception e) {
-        return buildResponse(req, res, e, null);
+        return buildResponse(req, res, e, null ,null);
     }
 
-    public Map<String, Object> buildResponse(HttpServletRequest req, HttpServletResponse res, Exception e, Integer status) {
+    public Map<String, Object> buildResponse(HttpServletRequest req, HttpServletResponse res, Exception e, Integer status, List<String> messages) {
         Map<String, Object> errorMap = new HashMap<>();
 
         if (status != null) {
@@ -35,9 +42,12 @@ public class CommonExceptionHandler {
             res.setStatus(httpStatus);
         }
 
+        List<String> optionalMessages = messages == null ? Collections.singletonList(e.getMessage()) : messages;
+
         errorMap.put("error", e.toString());
         errorMap.put("path", req.getRequestURL());
         errorMap.put("message", e.getMessage());
+        errorMap.put("messages", optionalMessages);
         errorMap.put("timestamp", System.currentTimeMillis());
 
         return errorMap;
